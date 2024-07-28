3149d5e62b90499973e4a21c3bd3bdd063977153
Junhyuk <ujunhyuk310@gmail.com>
2024-07-28 19:13:06 +0900

    불덩이 이펙트

--- Assets/02.Scripts/Enemy/Enemy Fireball.cs
+++ Assets/02.Scripts/Enemy/Enemy Fireball.cs
@@ -12,8 +12,11 @@ public class EnemyFireball : MonoBehaviour
     public float waitTime = 2f; // 방향 전환 전에 기다릴 시간
     public float jumpForce = 5f;
     public GameObject fireParticlePrefab; // 불길 이펙트 프리팹
+    public GameObject exprosionParticlePrefab;
+    public GameObject bigFireParticlePrefab;
     public int poolSize = 10; // 객체 풀의 크기
     public float particleLifetime = 2f; // 파티클 생존 시간
+    public float explosionRadius = 1.5f;
 
     private Queue<GameObject> particlePool;
     private bool isWaiting = false;
@@ -51,14 +54,23 @@ public class EnemyFireball : MonoBehaviour
             }
         }
 
-        // 레이캐스트를 시각화
-        Debug.DrawRay(groundCheck.position, Vector2.down * 0.6f, Color.blue);
-
-        RaycastHit2D jumpInfo = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.6f, groundLayer);
-        if (jumpInfo.collider == true)
+        if (!isWaiting)
         {
-            Fire();
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            // 레이캐스트를 시각화
+            Debug.DrawRay(groundCheck.position, Vector2.down * 0.6f, Color.blue);
+
+            RaycastHit2D jumpInfo = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.6f, groundLayer);
+            if (jumpInfo.collider == true)
+            {
+                Fire();
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+        }
+
+            //테스트
+            if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Explode());
         }
     }
 
@@ -84,7 +96,9 @@ public class EnemyFireball : MonoBehaviour
         GameObject particle = GetPooledObject();
         if (particle != null)
         {
-            particle.transform.position = transform.position;
+            Vector3 fireposition = new Vector3(transform.position.x, transform.position.y - 0.2f, transform.position.z);
+
+            particle.transform.position = fireposition;
             particle.transform.rotation = transform.rotation;
             particle.SetActive(true);
             StartCoroutine(DestroyAfterTime(particle, particleLifetime)); // 2초 후에 비활성화
@@ -121,8 +135,31 @@ public class EnemyFireball : MonoBehaviour
         // 파티클을 비활성화하고 객체 풀로 다시 추가
         ReturnToPool(obj);
     }
-    public void Explode()
+    private IEnumerator Explode()
     {
+        isWaiting = true;
+        // 점화 시 2.5초 후 반경 3m의 폭발
+        yield return new WaitForSeconds(2.5f);
 
+        Vector3 fireposition2 = new Vector3(transform.position.x , transform.position.y - 0.2f, transform.position.z);
+
+        GameObject exprosion = Instantiate(exprosionParticlePrefab, transform.position, Quaternion.identity);
+        GameObject fire = Instantiate(bigFireParticlePrefab, fireposition2, Quaternion.identity);
+
+        // 1초 후에 particle을 삭제
+        Destroy(exprosion, 1f);
+        Destroy(fire, 1f);
+
+
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+        foreach (Collider2D hitCollider in hitColliders)
+        {
+            if (hitCollider.CompareTag("Player"))
+            {
+                // 여기서 폭발 효과와 데미지 적용               
+            }
+        }
+        // 이 오브젝트를 파괴
+        Destroy(gameObject);
     }
 }
